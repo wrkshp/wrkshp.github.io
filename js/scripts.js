9a38ed4c632e11ef1c6f983b39469efc9912cdfe
Jackie Tseng <jactseng@amazon.com>
2019-06-03 21:41:22 -0700

    4059b13 readd the code steps

---
+++ js/scripts.js
@@ -0,0 +1,56 @@
+// THEME JS -- NOT SURE IF I NEED THIS LOL
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 60)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+})(jQuery); // End of use strict
+
+
+// FOR TABBED VIEWS
+$(document).ready(function(){
+    
+    $('ul.tabs li').click(function(){
+        var tab_id = $(this).attr('data-tab');
+
+        $('ul.tabs li').removeClass('current');
+        $('.tab-content').removeClass('current');
+
+        $(this).addClass('current');
+        $("."+tab_id).addClass('current');
+    })
+
+});
+
+
+// FOR SCROLLSPY
+$('.scrollspy-nav li a').click(function(event) {
+    event.preventDefault();
+    $($(this).attr('href'))[0].scrollIntoView({behavior: "smooth"});
+    scrollBy(0, -50);
+});
